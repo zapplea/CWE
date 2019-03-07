df3db62d8b48e940dbe17e9046cd096bd1b5d2c1
yibing <zapplea@gmail.com>
2019-03-07 15:19:26 +0800

    add

--- learn.py
+++ learn.py
@@ -1,8 +1,34 @@
 import tf_glove
+from os import listdir
+from os.path import isfile, join
+import pickle
+import numpy as np
 
-corpus = [["this", "is", "a", "comment", "."], ["this", "is", "another", "comment", "."]]
+def read_corpus(config):
+    rootpath = config['corpus']['rootpath']
+    files_name = [f for f in listdir(rootpath) if isfile(join(rootpath, f))]
+    corpus = []
+    for fname in files_name:
+        with open(join(rootpath,fname),'rb') as f:
+            data = pickle.load(f)
+            corpus.append(data)
+    corpus = np.concatenate(corpus,axis=0)
+    return corpus
 
-model = tf_glove.GloVeModel(embedding_size=300,context_size=3)
-model.fit_to_corpus(corpus)
-model.train(num_epochs=100)
-print(model.embedding_for("this"))
+def train(corpus,config):
+    # corpus = [["this", "is", "a", "comment", "."], ["this", "is", "another", "comment", "."]]
+
+    model = tf_glove.GloVeModel(embedding_size=config['model']['emb_size'],context_size=config['model']['n_gram'])
+    model.fit_to_corpus(corpus)
+    model.train(num_epochs=config['train']['num_epochs'])
+    print(model.embedding_for("this"))
+
+def main():
+    config = {'corpus':{'rootpath':'/datastore/liu121/sentidata2/data/meituan_jieba'},
+              'model':{'emb_size':'',
+                       'n_gram':3},
+              'train':{'num_epochs':100}
+              }
+    corpus = read_corpus(config)
+    print(corpus.shape)
+    print(corpus[0])
