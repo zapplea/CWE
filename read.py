27f105b5a32411dc96cacb6b1ce0d6b9f6013115
yibing <zapplea@gmail.com>
2019-03-08 16:52:03 +0800

    add

--- read.py
+++ read.py
@@ -7,3 +7,5 @@ with open('/datastore/liu121/charEmb/aic2018cwe_charEmb.pkl','rb') as f:
 with open('/datastore/liu121/wordEmb/aic2018cwe_wordEmb.pkl','rb') as f:
     dic = pickle.load(f)
     print(dic.keys())
+    print(dic['char_to_id'])
+    print(dic['char_embeddings'])
